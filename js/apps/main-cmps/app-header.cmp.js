573424ed130f063da725b0b5a78f6bfce7a61546
Idanatiya <idanatiya122@gmail.com>
2020-11-04 12:24:55 +0200

    added something

--- js/apps/main-cmps/app-header.cmp.js
+++ js/apps/main-cmps/app-header.cmp.js
@@ -1,22 +1,31 @@
 export default {
     name: 'appHeader',
     template: `
-                <header class="app-header">
-                    <div class="brand">
-                        <img src="assets/main-app/img/logo.png"/> AppSus
-                    </div>
-                    <nav class="main-nav clean-list">
-                        <li>
-                            <router-link to="/" exact>Home</router-link>
-                        </li>
-                        <li>
-                            <router-link to="/keep" exact>Keep App</router-link>
-                        </li>
-                        <li>
-                            <router-link to="/mail" exact>Mail App</router-link>
-                        </li> 
-                    </nav>
-                
-                </header>
-            `
+                <section>
+                    <!-- <button class="btn toggle-btn" @click="isShowNav = !isShowNav">
+                        <i class="fas fa-bars fa-2x"></i>
+                     </button> -->
+                    <header class="app-header" v-if="isShowNav">
+                        <div class="brand">
+                            <img src="assets/main-app/img/logo.png"/> AppSus
+                        </div>
+                        <nav class="main-nav clean-list">
+                            <li>
+                                <router-link to="/" exact>Home</router-link>
+                            </li>
+                            <li>
+                                <router-link to="/keep" exact>Keep App</router-link>
+                            </li>
+                            <li>
+                                <router-link to="/mail" exact>Mail App</router-link>
+                            </li> 
+                        </nav>
+                    </header>
+                </section>
+            `,
+    data() {
+        return {
+            isShowNav: true
+        }
+    },
 }
