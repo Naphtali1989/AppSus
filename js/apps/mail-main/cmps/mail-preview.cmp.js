f24f13ce6554e1ba04eceeecc29480b55b67bf5d
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-11-04 13:28:10 +0200

    button closing tag

--- js/apps/mail-main/cmps/mail-preview.cmp.js
+++ js/apps/mail-main/cmps/mail-preview.cmp.js
@@ -2,7 +2,7 @@ export default {
     props: ['mail'],
     template: `
             <section class="mail-preview">
-                <button class="prioritize-btn" @click="">STAR<button>
+                <button class="prioritize-btn" @click="">STAR</button>
                     <div class="mail-preview-subject">{{mail.subject}}</div>
                     <div class="mail-preview-body">{{mail.body}}</div>
             </section>
