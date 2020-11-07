41cc7e20e92331e26d0a8d783fe6f75b71d16582
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-11-07 23:53:27 +0200

    added long text to notes

--- js/apps/note/cmps/note-txt.cmp.js
+++ js/apps/note/cmps/note-txt.cmp.js
@@ -7,11 +7,18 @@ export default {
             <section class="note-txt">
                   <!-- <long-text :txt="note.info.val" /> -->
                   <p contenteditable="true" class="editable-title"   @blur="emitChange">{{note.info.title}}</p>
-                  <h3 contenteditable="true" class="editable-val"   @blur="emitChange">{{note.info.val}}</h3>
+                  <!-- <h3 contenteditable="true" class="editable-val"   @blur="emitChange">{{note.info.val}}</h3> -->
+                  <h3 contenteditable="true" class="editable-val"   @blur="emitChange">{{showCharacters}}</h3>
+                  <button @click="isShowMore = !isShowMore" class="show-txt-btn btn">{{btnTxt}}</button>
             </section>
 
 
     `,
+    data() {
+        return {
+            isShowMore: false
+        }
+    },
     methods: {
         emitChange(ev) {
             // console.log('event:', ev)
@@ -19,6 +26,18 @@ export default {
             if (ev.target.className === 'editable-val') this.note.info.val = ev.target.textContent
             else this.note.info.title = ev.target.textContent;
             this.$emit('update', this.note.id)
+        },
+    },
+    computed: {
+        showCharacters() {
+            // this.btnTxt = this.isShowMore ? 'Show Less' : 'Show More...';
+            if (!this.isShowMore && this.note.info.val.length > 30) return this.note.info.val.substring(0, 30)
+            else return this.note.info.val;
+        },
+        btnTxt() {
+            console.log(this.isShowMore)
+            if (this.isShowMore) return 'Show less'
+            else return 'Show More...'
         },
     },
     components: {
